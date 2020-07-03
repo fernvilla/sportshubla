b290ffec1f3681524ab017ea0a5f1ae2292da6db
Fernando Villasenor <fvilla18@gmail.com>
2020-07-03 00:11:19 -0700

    Create src folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+// import Link from 'next/link'
+
+export default function Home() {
+  return <div>Home</div>;
+  // return (
+  //   <ul>
+  //     <li>
+  //       <Link href="/a" as="/a">
+  //         <a>a</a>
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link href="/b" as="/b">
+  //         <a>b</a>
+  //       </Link>
+  //     </li>
+  //   </ul>
+  // )
+}
