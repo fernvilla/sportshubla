2e1cc66e509614fcc5693f9042ac6da9dfcb89fb
Fernando Villasenor <fvilla18@gmail.com>
2020-08-09 22:48:50 -0700

    Fix teams routes

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.use('/feeditemstypes', require('./feedItemTypes'));
 router.use('/leagues', require('./leagues'));
 router.use('/newssources', require('./newsSources'));
 router.use('/rssfeeds', require('./rssFeeds'));
+router.use('/teams', require('./teams'));
 
 module.exports = router;
