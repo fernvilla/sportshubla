b6fa5787f1c3abc0b875c96d9e229da6be2638f0
Fernando Villasenor <fvilla18@gmail.com>
2020-10-10 00:06:00 -0700

    Fix word break

--- client/src/components/tweets/Tweet.tsx
+++ client/src/components/tweets/Tweet.tsx
@@ -52,7 +52,7 @@ const Tweet = ({ tweet, displayTeamLink = false }: Props) => {
             isExternal
             color="blue.700"
           >
-            <Text m={0} lineHeight="shorter" fontWeight="medium">
+            <Text m={0} lineHeight="shorter" fontWeight="medium" wordBreak="break-all">
               {tweet.text}
             </Text>
           </Link>
